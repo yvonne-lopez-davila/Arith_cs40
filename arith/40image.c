56ccd6dc46432cdd0e92d555189c4c61bb371e2c
yvonne-lopez-davila <yvonne.lopez_davila@tufts.edu>
2024-03-03 12:28:47 -0500

    ppm_diff done and compression steps 1&2
    *Bug in trimming array step when setting rgb fields

--- arith/40image.c
+++ arith/40image.c
@@ -1,10 +1,42 @@
+/*
+ *     40image.c
+ *     Authors: Yvonne Lopez Davila and Caroline Shaw
+ *     ylopez02 and cshaw03
+ *     HW4 / Arith
+ *
+ *     TO DO:
+ *
+ *      Usage: 
+ *          1) 40image -d [filename]
+ *            (Decompress filename)
+ *
+ *          2) 40image -c [filename]
+ *            (Compress filename)
+ * 
+ *     Notes:
+ *
+ */
+ 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "assert.h"
 #include "compress40.h"
+#include "pnm.h"
+#include "a2methods.h"
+#include "a2plain.h"
+#include "a2blocked.h"
+
 
 static void (*compress_or_decompress)(FILE *input) = compress40;
+
+void apply(int col, int row, A2Methods_UArray2 trim_arr, void *elem, void *og_img);
+void compress40 (FILE *input);
+void decompress40 (FILE *input);
+Pnm_ppm initialize_ppm(FILE *input);
+bool is_even(int x);
+void trim_odd_dims(Pnm_ppm image, int width, int height);
 
 int main(int argc, char *argv[])
 {
@@ -39,4 +71,99 @@ int main(int argc, char *argv[])
         }
 
         return EXIT_SUCCESS; 
+}
+
+// Compress40 TODO (function contract)
+void compress40 (FILE *input)
+{
+        //grid of RGB int pixels todo del
+        /* Initialize and populate pnm for uncompressed file */ 
+        Pnm_ppm uncompressed = initialize_ppm(input);
+        trim_odd_dims(uncompressed, uncompressed->width, uncompressed->height);
+
+        printf("W: %d \n ", uncompressed->width);
+        printf("H: %d \n ", uncompressed->height);
+}
+
+// Decompress40 TODO (function contract)
+void decompress40 (FILE *input)
+{
+        (void) input;       
+
+}
+
+//trim_odd_dims TODO (function contract)
+void trim_odd_dims(Pnm_ppm image, int width, int height)
+{
+        /* Set width and height to trimmed dimensions */
+        if (is_even(width) && is_even(height))
+        {
+                return;
+        }
+        if (!is_even(width))
+        {
+                width--;
+        }
+        if (!is_even(height))
+        {  
+                height--;
+        }
+
+        printf("testing width pre : %d \n", image->width); 
+        printf("testing height pre: %d \n", image->height); 
+
+        /* Initialize a new 2D array with new dims */
+        A2Methods_UArray2 trim_arr = image->methods->new(width, height, sizeof(struct Pnm_rgb));
+
+        /* Populate with data from old array */
+        A2Methods_mapfun *map = image->methods->map_default;
+        map(trim_arr, apply, image);
+
+        /* Swap arrays and free untrimmed */
+        Pnm_ppm temp = image->pixels;
+        image->pixels = trim_arr;
+        Pnm_ppmfree(&temp);
+
+        printf("testing width after : %d \n", image->width); 
+        printf("testing height after: %d \n", image->height); 
+
+}
+
+// apply TODO (function contract)
+void apply(int col, int row, A2Methods_UArray2 trim_arr, void *elem, void *og_img)
+{
+        Pnm_ppm orig = (Pnm_ppm)og_img;
+
+        /* Get element from orig img arr analagous position */
+        Pnm_rgb *pix_val = orig->methods->at(orig->pixels, col, row);
+
+        /* Copy RGB values to pix in trimmed array */
+        Pnm_rgb curr_pix = (struct Pnm_rgb *) elem;
+        printf("\nTRACE 1\n");
+        // todo these lines are seg-faulting
+        curr_pix->red = (*pix_val)->red;
+        curr_pix->green = (*pix_val)->green;
+        curr_pix->blue = (*pix_val)->blue;
+
+        (void) trim_arr;        
+}
+
+bool is_even(int x)
+{
+        return (x % 2 == 0);
+}
+
+//Initialize_ppm TODO (function contract)
+Pnm_ppm initialize_ppm(FILE *input)
+{
+        printf("TRACE 2\n");
+        assert(input != NULL);
+        
+        /* default to UArray2 methods */
+        A2Methods_T methods = uarray2_methods_plain;
+        assert(methods != NULL);
+
+        Pnm_ppm image = Pnm_ppmread(input, methods);
+
+        return image;
 }
--- arith/ppmdiff.c
+++ arith/ppmdiff.c
@@ -1,7 +1,7 @@
 /*
  *     ppmdiff.c
- *     Authors: Yvonne Lopez Davila and
- *     ylopez02 and
+ *     Authors: Yvonne Lopez Davila and Caroline Shaw
+ *     ylopez02 and cshaw03
  *     HW4 / Arith
  *
  *     The ppmdiff program quantifies the similarity between two user-specified 
@@ -46,9 +46,11 @@ void open_valid_ppm_files(int argc, char *argv[], FILE **one_input,
 
 struct z_info
 {
-        A2Methods_UArray2 imgBig_arr;
+        Pnm_ppm imgBig;
+        unsigned maxval_small;
         A2Methods_T methods;
         float sum;
+        float denom;
 };
 
 int main(int argc, char *argv[])
@@ -57,7 +59,8 @@ int main(int argc, char *argv[])
         FILE *two_input;
         bool big_first = true;
 
-        // todo test with diff combos of stdin
+        // todo test with diff combos of stdin//
+        
         /* Updates file pointers for valid file input */
         open_valid_ppm_files(argc, argv, &one_input, &two_input);
 
@@ -73,22 +76,32 @@ int main(int argc, char *argv[])
         check_width_height_diff(&image_1, &image_2, &big_first);
         
         struct z_info *info = malloc(sizeof(struct z_info)); // todo free
+        assert(info != NULL);
         info->sum = 0;
         info->methods = methods;
+        info->denom = 0;
 
         /* Iterate through smaller img and calc cum. sum of squared diffs */
         if (big_first) {
-                info->imgBig_arr = image_1->pixels;
+                info->imgBig = image_1;
+                info->maxval_small = image_2->denominator;
                 map(image_2->pixels, get_zDiff, info);
         }
         else {
-                info->imgBig_arr = image_2->pixels;
+                info->imgBig = image_2;
+                info->maxval_small = image_1->denominator;
                 map(image_1->pixels, get_zDiff, info);
         }
 
-        printf("Z coefficient is: %.4f\n", info->sum);
+        float z = sqrt(info->sum / info->denom);
 
-        return 0;
+        free(info);
+        Pnm_ppmfree(&image_1);
+        Pnm_ppmfree(&image_2);
+
+        printf("%.4f\n", z);
+
+        return EXIT_SUCCESS;
 }
 
 
@@ -101,18 +114,23 @@ void get_zDiff(int col, int row, A2Methods_UArray2 imgSmall_arr, void *elem, voi
         Pnm_rgb small_pix = (Pnm_rgb)elem;
 
         /* Get analagous pixel in larger photo */
-        Pnm_rgb big_pix = info->methods->at(info->imgBig_arr, col, row);
+        Pnm_rgb big_pix = info->methods->at(info->imgBig->pixels, col, row);
         
-        unsigned r_diff = (big_pix->red - (small_pix)->red);
-        unsigned g_diff = (big_pix->green - (small_pix)->green);
-        unsigned b_diff = (big_pix->blue - (small_pix)->blue);
-        
-        unsigned numer = (r_diff * r_diff) + (g_diff * g_diff) + (b_diff * b_diff);
-        unsigned denom = (3 * info->methods->width(imgSmall_arr) * info->methods->height(imgSmall_arr));
+        float r_diff = ((float)(big_pix->red) / (float)
+        (info->imgBig->denominator) - ((float)(small_pix->red) / (float)
+        (info->maxval_small)));
 
-        info->sum += sqrt(numer / denom ); // todo check type, might need to be a float
-
-        printf("Tracing curr sum... (col, row): (%d, %d), sum: %.4f \n", col, row, info->sum);
+        float g_diff = ((float)(big_pix->green) / (float)
+        (info->imgBig->denominator) - ((float)(small_pix->green) / (float)
+        (info->maxval_small)));
+        float b_diff = ((float)(big_pix->blue) / (float)(info 
+        ->imgBig->denominator) - ((float)(small_pix->blue) / (float)
+        (info->maxval_small)));
+       
+        float numer = (r_diff * r_diff) + (g_diff * g_diff) + (b_diff * b_diff);
+   
+        info->denom = (3 * info->methods->width(imgSmall_arr) * info->methods->height(imgSmall_arr));
+        info->sum += numer;
 
 }
 
@@ -126,8 +144,6 @@ void check_width_height_diff(Pnm_ppm *img_one, Pnm_ppm *img_two, bool *big_first
 
         }
 
-//        printf("Testing swap: %d\n", (*img_one)->width);
-
         if (abs(diff) > MAX_DIFF) {
                 fprintf(stderr, "Error: Difference is too large\n");
                 float one = 1.0;
@@ -136,7 +152,7 @@ void check_width_height_diff(Pnm_ppm *img_one, Pnm_ppm *img_two, bool *big_first
 
 }
 
-        //Optionally, one or the other argument (but not both) may be the C string "-", which stands for standard input.
+        /* Optionally, one or the other argument (but not both) may be the C string "-", which stands for standard input. */
 void open_valid_ppm_files(int argc, char *argv[], FILE **one_input, FILE 
                           **two_input)
 {
